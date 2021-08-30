6455787272352f1120a0efb9e20dc3463b1de510
happy-go-luck <89762622+happy-go-luck@users.noreply.github.com>
2021-08-30 13:23:05 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const hText = document.getElementById('h-text');
+const hTare = document.getElementById('h-tare');
+
+
+
+function text() {
+  hText.style.width = '80%';
+}
+
+
+
+setTimeout(text);
